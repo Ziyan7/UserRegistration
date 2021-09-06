c0d52a564cfe8a23cc918c52ca2a235ba4035417
Your Name <you@example.com>
2021-09-06 21:38:03 +0530

    Added: method to last name

--- src/main/java/userRegistration/regex/UserRegistration.java
+++ src/main/java/userRegistration/regex/UserRegistration.java
@@ -3,12 +3,18 @@ package userRegistration.regex;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-	//method to validate the first name
+	// method to validate the first name
 
-    public boolean validateFirstName(String firstName)
-    {
-        String pattern = "^[A-Z][a-zA-Z]{2,}";
-        return Pattern.matches(pattern, firstName);
-    }
-    
+	public boolean validateFirstName(String firstName) {
+		String pattern = "[A-Z][a-zA-Z]{2,}";
+		return Pattern.matches(pattern, firstName);
+	}
+
+	// method to validate the Last name
+
+	public boolean validateLastName(String lastName) {
+		String pattern = "[A-Z][a-zA-Z]{2,}";
+		return Pattern.matches(pattern, lastName);
+	}
+
 }
--- src/test/java/userRegistration/regex/UserRegistrationTest.java
+++ src/test/java/userRegistration/regex/UserRegistrationTest.java
@@ -6,18 +6,20 @@ import java.util.Scanner;
 
 import org.junit.Test;
 
-
-
 public class UserRegistrationTest {
 	@Test
-	//testing method for valid input name
-	public void test() {
-		System.out.println("Enter your Name");
+	// testing method for valid input name
+	public void firstNameChecker() {
+		System.out.println("Enter your First Name");
 		Scanner sc = new Scanner(System.in);
-		String name= sc.nextLine();
+		String firstname = sc.nextLine();
+
+		System.out.println("Enter your Last Name");
+		String lastname = sc.nextLine();
 		sc.close();
 		UserRegistration obj = new UserRegistration();
-		assertTrue(obj.validateFirstName(name));
+		assertTrue(obj.validateFirstName(firstname));
+		assertTrue(obj.validateLastName(lastname));
 	}
-	
+
 }
